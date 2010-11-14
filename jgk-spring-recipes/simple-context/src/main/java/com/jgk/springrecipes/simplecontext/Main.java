a8fe46d1bf41e34661e4019199ff1a76bf3b5936
javapda <javapda@gmail.com>
2010-11-14 20:55:39 +0000

    add entry

---
+++ jgk-spring-recipes/simple-context/src/main/java/com/jgk/springrecipes/simplecontext/Main.java
@@ -0,0 +1,13 @@
+package com.jgk.springrecipes.simplecontext;
+
+/**
+ * Hello!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello jgk-spring-recipes!" );
+    }
+}
