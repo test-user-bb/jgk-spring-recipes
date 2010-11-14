23711b3e5f3fd97c161ee4e7f30b7e7edbcd702a
javapda <javapda@gmail.com>
2010-11-14 21:04:40 +0000

    add super simple bean

--- jgk-spring-recipes/simple-context/src/main/java/com/jgk/springrecipes/simplecontext/Main.java
+++ jgk-spring-recipes/simple-context/src/main/java/com/jgk/springrecipes/simplecontext/Main.java
@@ -1,13 +1,29 @@
 package com.jgk.springrecipes.simplecontext;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.jgk.springrecipes.simplecontext.beans.SuperSimpleBean;
+
 /**
  * Hello!
  *
  */
 public class Main 
 {
+	public void init() {
+		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {
+		"classpath:com/jgk/springrecipes/simplecontext/simple-context-application-config.xml"});
+		SuperSimpleBean ssb1 = context.getBean(SuperSimpleBean.class);
+		ssb1.doSomething();
+		SuperSimpleBean ssb2 = (SuperSimpleBean) context.getBean("superSimpleBean");
+		ssb2.doSomething();
+		System.out.println(ssb1==ssb2);
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello jgk-spring-recipes!" );
+        new Main().init();
+        
     }
 }
