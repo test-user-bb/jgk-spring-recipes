bd2646bc99092bf154656cad7991458a24c22f00
javapda@gmail.com <javapda@gmail.com@c572b21e-de6c-5e44-9ac8-d7cb41f125f7>
2010-11-18 12:08:27 +0000

    add arrays converter demo

--- trunk/jgk-spring-recipes/simple-converters/src/test/java/com/jgk/springrecipes/simple/converters/ArraysConverterDemoTest.java
+++ trunk/jgk-spring-recipes/simple-converters/src/test/java/com/jgk/springrecipes/simple/converters/ArraysConverterDemoTest.java
@@ -1,6 +1,7 @@
 package com.jgk.springrecipes.simple.converters;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,19 +10,17 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"/com/jgk/springrecipes/simple/converters/PrimitivesConverterDemoTest-context.xml"})//({"/applicationContext.xml", "/applicationContext-test.xml"})
-public class PrimitivesConverterDemoTest {
+@ContextConfiguration({"/com/jgk/springrecipes/simple/converters/ArraysConverterDemoTest-context.xml"})//({"/applicationContext.xml", "/applicationContext-test.xml"})
+public class ArraysConverterDemoTest {
 	@Autowired
     private ApplicationContext applicationContext;
 	
 	@Test
-	
 	public void someTest() {
-		PrimitivesConverterDemo pcd = applicationContext.getBean(PrimitivesConverterDemo.class);
-		assertNotNull(pcd);
-		System.out.println(pcd);
+		ArraysConverterDemo acd = applicationContext.getBean(ArraysConverterDemo.class);
+		assertNotNull(acd);
+		System.out.println(acd);
 	}
 
 }
