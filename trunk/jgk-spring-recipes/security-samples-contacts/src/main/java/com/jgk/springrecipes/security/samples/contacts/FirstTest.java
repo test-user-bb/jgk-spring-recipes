c6c4db6aeda92ddeaddae0804a0962b608a065c6
javapda@gmail.com <javapda@gmail.com@c572b21e-de6c-5e44-9ac8-d7cb41f125f7>
2010-11-29 19:38:48 +0000

    add sample spring security application 'contacts' actually from SpringSource - adapted to run now

---
+++ trunk/jgk-spring-recipes/security-samples-contacts/src/main/java/com/jgk/springrecipes/security/samples/contacts/FirstTest.java
@@ -0,0 +1,14 @@
+package com.jgk.springrecipes.security.samples.contacts;
+
+//import static org.junit.Assert.*;
+//import org.junit.Test;
+
+
+public class FirstTest {
+	
+//	@Test
+	public void checkIt() {
+		First first = new First();
+//		assertNotNull(first);
+	}
+}
