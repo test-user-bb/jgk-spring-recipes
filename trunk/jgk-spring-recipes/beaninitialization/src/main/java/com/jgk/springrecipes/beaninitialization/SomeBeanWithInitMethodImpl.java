40d1c2674e5e83e3d864add45928c441fd35e80b
javapda@gmail.com <javapda@gmail.com@c572b21e-de6c-5e44-9ac8-d7cb41f125f7>
2010-11-29 04:18:40 +0000

    remove

--- trunk/jgk-spring-recipes/beaninitialization/src/main/java/com/jgk/springrecipes/beaninitialization/SomeBeanWithInitMethodImpl.java
+++ trunk/jgk-spring-recipes/beaninitialization/src/main/java/com/jgk/springrecipes/beaninitialization/SomeBeanWithInitMethodImpl.java
@@ -2,6 +2,7 @@ package com.jgk.springrecipes.beaninitialization;
 
 public class SomeBeanWithInitMethodImpl {
 
+	// NOTE: init method must have NO ARGUMENTS
 	public void firstInit() {
 		System.out.println("FIRST INIT TIME");
 	}
