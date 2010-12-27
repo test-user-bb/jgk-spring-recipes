868e0e526860fe6dd5296e3bf913391b3a3e8488
javapda <javapda@gmail.com>
2010-12-27 03:00:14 +0000

    demonstrate queryForInt

--- jgk-spring-recipes/jdbc/src/test/java/com/jgk/springrecipes/jdbc/embedded/HsqldbEmbeddedProgrammaticTest.java
+++ jgk-spring-recipes/jdbc/src/test/java/com/jgk/springrecipes/jdbc/embedded/HsqldbEmbeddedProgrammaticTest.java
@@ -1,29 +1,28 @@
 package com.jgk.springrecipes.jdbc.embedded;
 
 import static org.junit.Assert.*;
+
+import javax.sql.DataSource;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 public class HsqldbEmbeddedProgrammaticTest {
-	
+	private EmbeddedDatabase db;
 	
 	@Test
 	public void basis() {
-		System.out.println("BASIS");
-		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.HSQL)
-				.addScript("/com/jgk/springrecipes/jdbc/embedded/hsqldb-schema.sql")
-				.addScript("/com/jgk/springrecipes/jdbc/embedded/hsqldb-test-data.sql").build();
-		assertNotNull(db);
-	    // do stuff against the db (EmbeddedDatabase extends javax.sql.DataSource)
-	    db.shutdown();
-	    
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(db);
+		Integer rowCount = jdbcTemplate.queryForInt("select count(*) from t_person");
+		assertNotNull(rowCount);
+		assertEquals(Integer.valueOf(3), rowCount);
 	}
 	
 	@BeforeClass
@@ -38,12 +37,18 @@ public class HsqldbEmbeddedProgrammaticTest {
 
 	@Before
 	public void setUp() throws Exception {
-		System.out.println("BEFORE");
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		db = builder.setType(EmbeddedDatabaseType.HSQL)
+				.addScript("/com/jgk/springrecipes/jdbc/embedded/hsqldb-schema.sql")
+				.addScript("/com/jgk/springrecipes/jdbc/embedded/hsqldb-test-data.sql").build();
+		assertNotNull(db);
+		assertTrue(db instanceof DataSource);
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		System.out.println("AFTER");
+	    // do stuff against the db (EmbeddedDatabase extends javax.sql.DataSource)
+	    db.shutdown();
 	}
 
 }
