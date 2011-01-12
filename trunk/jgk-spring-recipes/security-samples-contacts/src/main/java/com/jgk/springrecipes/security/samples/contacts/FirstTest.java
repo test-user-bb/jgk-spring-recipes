4aec76b145f691636987f1feb5368dd458a76f95
javapda <javapda@c572b21e-de6c-5e44-9ac8-d7cb41f125f7>
2011-01-12 04:18:18 +0000

    enable test

--- trunk/jgk-spring-recipes/security-samples-contacts/src/main/java/com/jgk/springrecipes/security/samples/contacts/FirstTest.java
+++ trunk/jgk-spring-recipes/security-samples-contacts/src/main/java/com/jgk/springrecipes/security/samples/contacts/FirstTest.java
@@ -1,12 +1,14 @@
 package com.jgk.springrecipes.security.samples.contacts;
 
+import org.junit.Test;
+
 //import static org.junit.Assert.*;
 //import org.junit.Test;
 
 
 public class FirstTest {
 	
-//	@Test
+	@Test
 	public void checkIt() {
 		First first = new First();
 //		assertNotNull(first);
