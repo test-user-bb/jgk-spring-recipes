a4c759a18bdcd791807e3e0d8832f8089f15b678
javapda@gmail.com <javapda@gmail.com@c572b21e-de6c-5e44-9ac8-d7cb41f125f7>
2011-01-26 21:24:03 +0000

    update resources

---
+++ trunk/jgk-spring-recipes/orm/springjpaminimalist/src/main/java/com/jgk/springrecipes/orm/springjpaminimalist/repository/LogMessageRepository.java
@@ -0,0 +1,12 @@
+package com.jgk.springrecipes.orm.springjpaminimalist.repository;
+
+import java.util.List;
+
+import com.jgk.springrecipes.orm.springjpaminimalist.domain.LogMessage;
+
+public interface LogMessageRepository {
+	List<LogMessage> findAll();
+
+	void save(LogMessage lm);
+	
+}
