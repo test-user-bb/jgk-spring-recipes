cbc698efbe2cda564e65f17f00f9bfb08d8d1fa4
javapda <javapda@gmail.com>
2011-01-27 00:54:24 +0000

    update files with log message

--- jgk-spring-recipes/orm/springjpaminimalist/src/main/java/com/jgk/springrecipes/orm/springjpaminimalist/repository/JpaLogMessageRepository.java
+++ jgk-spring-recipes/orm/springjpaminimalist/src/main/java/com/jgk/springrecipes/orm/springjpaminimalist/repository/JpaLogMessageRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,26 +13,20 @@ import com.jgk.springrecipes.orm.springjpaminimalist.domain.LogMessage;
 @Repository
 public class JpaLogMessageRepository implements LogMessageRepository {
 	private EntityManager entityManager;
-	
+
 	@PersistenceContext
 	public void setEntityManager(EntityManager entityManager) {
 		this.entityManager = entityManager;
 	}
 
 	public List<LogMessage> findAll() {
-		return entityManager.createQuery("select p from LogMessage p",LogMessage.class).getResultList();
+		return entityManager.createQuery("select p from LogMessage p",
+				LogMessage.class).getResultList();
 	}
 
 	@Override
-	public void save(LogMessage logMessage) {
-		
-		entityManager.persist(logMessage);
-		
-	}
-	
 	@Transactional
-	public void nothing() {
-		
+	public void save(LogMessage logMessage) {
+		entityManager.persist(logMessage);
 	}
-	
 }
