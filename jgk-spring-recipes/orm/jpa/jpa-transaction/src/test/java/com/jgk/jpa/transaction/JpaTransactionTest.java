44580bbf818c75e8e8786f323e779accd1aa0f93
javapda <javapda@gmail.com>
2011-01-30 06:41:02 +0000

    add persistence capability

---
+++ jgk-spring-recipes/orm/jpa/jpa-transaction/src/test/java/com/jgk/jpa/transaction/JpaTransactionTest.java
@@ -0,0 +1,41 @@
+package com.jgk.jpa.transaction;
+
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.jgk.jpa.transaction.domain.TradeData;
+import com.jgk.jpa.transaction.repository.TradeDataRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value="classpath:/com/jgk/jpa/transaction/JpaTransactionTest-context.xml")
+public class JpaTransactionTest {
+	
+	@Inject
+	ApplicationContext applicationContext;
+	
+	@Inject
+	TradeDataRepository tradeDataRepository;
+	
+	@Test
+	public void doit() {
+		System.out.println(applicationContext);
+		assertNotNull(applicationContext.getBean("tradeDataRepository"));
+		assertNotNull(applicationContext.getBean(TradeDataRepository.class));
+		System.out.println(applicationContext.getBean(TradeDataRepository.class));
+		TradeData td = new TradeData();
+		td.setTicker("IBM");
+		td.setAmount(243.23);
+		tradeDataRepository.makePersistent(td);
+		System.out.println(td.getId());
+		
+	}
+
+}
