de208c575bbb36aa37170f716adc63b7c1f803ef
javapda@gmail.com <javapda@gmail.com@c572b21e-de6c-5e44-9ac8-d7cb41f125f7>
2011-02-04 10:50:37 +0000

    add @RequestMapping entries

--- trunk/jgk-spring-recipes/mvc/mvc-jed/src/main/java/com/jgk/springrecipes/mvc/jed/controllers/HelloWorldController.java
+++ trunk/jgk-spring-recipes/mvc/mvc-jed/src/main/java/com/jgk/springrecipes/mvc/jed/controllers/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.jgk.springrecipes.mvc.jed.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller//(value="FunksterJoe")
@@ -13,4 +16,18 @@ public class HelloWorldController {
         mav.addObject("message", "Hello World!");
         return mav;
     }
+    @RequestMapping(value="/owners/{ownerId}", method=RequestMethod.GET)
+    public String findOwner(@PathVariable String ownerId, Model model) {
+//      Owner owner = ownerService.findOwner(ownerId);
+    	String owner = "Fred Flintstone";
+      model.addAttribute("owner", owner);  
+      return "displayOwner"; 
+    }    
+    @RequestMapping(value="/meds/bymedid/{medId}", method=RequestMethod.GET)
+    public String findMedName(@PathVariable("medId") String medId, Model model) {
+    	System.out.println("medId:"+medId);
+    	String medName="Aspirin";
+      model.addAttribute("medName", medName);  
+      return "displayMedName"; 
+    }    
 }
