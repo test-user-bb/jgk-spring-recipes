91cf8a504c1537abf994bc388fe1b97d37c874a2
javapda@gmail.com <javapda@gmail.com@c572b21e-de6c-5e44-9ac8-d7cb41f125f7>
2011-04-07 04:48:36 +0000

    add (spring data) hades replacement

---
+++ trunk/jgk-spring-recipes/orm/jpa/jpa-data/src/test/java/com/jgk/springrecipes/orm/jpa/data/MySpringJpaDataTest.java
@@ -0,0 +1,17 @@
+package com.jgk.springrecipes.orm.jpa.data;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations="classpath:/com/jgk/springrecipes/orm/jpa/data/MySpringJpaDataTest-config.xml")
+public class MySpringJpaDataTest {
+
+	@Test
+	public void testIt() {
+		assertTrue(true);
+	}
+}
