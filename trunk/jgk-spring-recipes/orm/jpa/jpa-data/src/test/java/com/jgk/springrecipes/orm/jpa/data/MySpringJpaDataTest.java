04e3bc80b55d5ba0c5405d43d42b87f218271379
javapda@gmail.com <javapda@gmail.com@c572b21e-de6c-5e44-9ac8-d7cb41f125f7>
2011-04-08 00:25:16 +0000

    update the library to M5 for common

--- trunk/jgk-spring-recipes/orm/jpa/jpa-data/src/test/java/com/jgk/springrecipes/orm/jpa/data/MySpringJpaDataTest.java
+++ trunk/jgk-spring-recipes/orm/jpa/jpa-data/src/test/java/com/jgk/springrecipes/orm/jpa/data/MySpringJpaDataTest.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations="classpath:/com/jgk/springrecipes/orm/jpa/data/MySpringJpaDataTest-config.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:/com/jgk/springrecipes/orm/jpa/data/MySpringJpaDataTest-config.xml")
 public class MySpringJpaDataTest {
 
 	@Test
