0b1836deb85e8c74839a3f9e50508cb7441852cd
javapda@gmail.com <javapda@gmail.com@c572b21e-de6c-5e44-9ac8-d7cb41f125f7>
2011-04-23 16:43:17 +0000

    add some general examples

--- trunk/jgk-spring-recipes/blackbelt/blackbelt.general/src/test/java/com/jgk/springrecipes/blackbelt/general/BlackbeltGeneralTest.java
+++ trunk/jgk-spring-recipes/blackbelt/blackbelt.general/src/test/java/com/jgk/springrecipes/blackbelt/general/BlackbeltGeneralTest.java
@@ -3,8 +3,13 @@ package com.jgk.springrecipes.blackbelt.general;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.jgk.springrecipes.blackbelt.general.beans.SomeBeanNotRequired;
+import com.jgk.springrecipes.blackbelt.general.beans.SomeBeanWithIndexedConstructor;
 
 import static org.junit.Assert.*;
 
@@ -14,6 +19,15 @@ import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:/BlackbeltGeneralTest-config.xml"})
 public class BlackbeltGeneralTest {
+	@Autowired
+	ApplicationContext applicationContext;
+	
+	@Autowired(required=true)
+	SomeBeanWithIndexedConstructor someBeanWithIndexedConstructor;
+	
+	@Autowired(required=false) 
+	SomeBeanNotRequired someBeanNotRequired;
+	
 	@BeforeClass
 	public static void onlyOnce() {
 //		System.out.println("Before starting");
