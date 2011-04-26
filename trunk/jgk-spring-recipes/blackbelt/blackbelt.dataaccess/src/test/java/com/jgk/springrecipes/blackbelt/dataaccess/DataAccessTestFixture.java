d74ca27b1cb7eb29d48e2497e1e7d91390f5c9c9
javapda@gmail.com <javapda@gmail.com@c572b21e-de6c-5e44-9ac8-d7cb41f125f7>
2011-04-26 20:42:23 +0000

    add some hibernate session factory testing

--- trunk/jgk-spring-recipes/blackbelt/blackbelt.dataaccess/src/test/java/com/jgk/springrecipes/blackbelt/dataaccess/DataAccessTestFixture.java
+++ trunk/jgk-spring-recipes/blackbelt/blackbelt.dataaccess/src/test/java/com/jgk/springrecipes/blackbelt/dataaccess/DataAccessTestFixture.java
@@ -1,5 +1,9 @@
 package com.jgk.springrecipes.blackbelt.dataaccess;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -10,10 +14,29 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(value="classpath:/DataAccessTest-config.xml")
 abstract public class DataAccessTestFixture {
+	protected boolean legalHost;
 	
+
 	@Autowired
 	ApplicationContext applicationContext;
 	
 	@Autowired 
 	JdbcTemplate jdbcTemplate;
+	
+	
+	@Before
+	public void checkHost() {
+		try {
+			String hostName = InetAddress.getLocalHost().getHostName();
+			String hostAddress = InetAddress.getLocalHost().getHostAddress();
+//			System.out.println("host name: " + hostName);
+//			System.out.println("host address: " + hostAddress);
+			legalHost=hostAddress.startsWith("192.168.6.");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 }
