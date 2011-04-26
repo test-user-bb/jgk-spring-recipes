4ae2a0614c59afdec77f16a47bae759521984cb3
javapda <javapda@gmail.com>
2011-04-26 21:37:37 +0000

    modify test

--- jgk-spring-recipes/blackbelt/blackbelt.dataaccess/src/test/java/com/jgk/springrecipes/blackbelt/dataaccess/HibernateTest.java
+++ jgk-spring-recipes/blackbelt/blackbelt.dataaccess/src/test/java/com/jgk/springrecipes/blackbelt/dataaccess/HibernateTest.java
@@ -4,9 +4,11 @@ import static org.junit.Assert.assertNotNull;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.jgk.springrecipes.blackbelt.dataaccess.domain.LegoBrick;
 import com.jgk.springrecipes.blackbelt.dataaccess.domain.Username;
 
 public class HibernateTest extends DataAccessTestFixture {
@@ -23,6 +25,17 @@ public class HibernateTest extends DataAccessTestFixture {
 			Username username = (Username) session.get(Username.class, "HAWKEYE");
 			assertNotNull(username);
 			//System.out.println(username);
+		}
+	}
+	
+	@Test
+	@Ignore // no table called LegoBrick :-)
+	public void testLegoBrickHibernate() {
+		if(legalHost) {
+			Session session = mySessionFactory.openSession();
+			LegoBrick legoBrick = (LegoBrick) session.get(LegoBrick.class, 23L);
+			assertNotNull(legoBrick);
+			System.out.println(legoBrick);
 		}
 	}
 	
