799dfefc3ad40e9b99ba75f22e077ffc260bc8d9
javapda@gmail.com <javapda@gmail.com@c572b21e-de6c-5e44-9ac8-d7cb41f125f7>
2011-08-07 18:38:58 +0000

    save data records

--- trunk/jgk-spring-rest/src/test/java/com/jgk/springrecipes/rest/json/JacksonTest.java
+++ trunk/jgk-spring-rest/src/test/java/com/jgk/springrecipes/rest/json/JacksonTest.java
@@ -6,20 +6,73 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.codehaus.jackson.JsonEncoding;
+import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.ObjectNode;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import com.jgk.springrecipes.rest.json.User.Gender;
 
+
+/**
+ * http://wiki.fasterxml.com/JacksonInFiveMinutes
+ * @author jkroub
+ *
+ */
 public class JacksonTest {
 	private ObjectMapper mapper;
+	private File userJsonFile;
+	private File streamingUserJsonFile;
 	@Before
 	public void setup() {
 		mapper = new ObjectMapper(); // can reuse, share globally
+		URL url = JacksonTest.class.getClassLoader().getResource("testdata/user.json");
+		System.out.println("FILE IS: " + url);
+		userJsonFile = new File(url.getFile());
+		url = JacksonTest.class.getClassLoader().getResource("testdata/streaming.user.json");
+		streamingUserJsonFile = new File(url.getFile());
 		
+	}
+	
+	@Ignore
+	@Test public void testStreaming() throws JsonGenerationException, IOException {
+		JsonFactory f = new JsonFactory();
+		JsonGenerator g = f.createJsonGenerator(streamingUserJsonFile,JsonEncoding.UTF8);
+
+		g.writeStartObject();
+		g.writeObjectFieldStart("name");
+		g.writeStringField("first", "Joe");
+		g.writeStringField("last", "Sixpack");
+		g.writeEndObject(); // for field 'name'
+		g.writeStringField("gender", Gender.MALE.toString());
+		g.writeBooleanField("verified", false);
+		g.writeFieldName("userImage"); // no 'writeBinaryField' (yet?)
+//		byte[] binaryData = ...;
+//		g.writeBinary(binaryData);
+		g.writeEndObject();
+		g.close(); // important: will force flushing of output, close underlying output stream
+	}
+	
+	@Test public void testTree() throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper m = new ObjectMapper();
+		// can either use mapper.readTree(JsonParser), or bind to JsonNode
+		JsonNode rootNode = m.readValue(userJsonFile, JsonNode.class);
+		// ensure that "last name" isn't "Xmler"; if is, change to "Jsoner"
+		JsonNode nameNode = rootNode.path("name");
+		String lastName = nameNode.path("last").getTextValue();
+		if ("xmler".equalsIgnoreCase(lastName)) {
+		  ((ObjectNode)nameNode).put("last", "Jsoner");
+		}
+		// and write it out:
+		m.writeValue(new File("target/user-modified-tree.json"), rootNode);
 	}
 
 	@Test
@@ -44,14 +97,42 @@ public class JacksonTest {
 	}
 	
 	@Test
-	public void testSimple() {
-		System.out.println(getJson());
-		URL url = JacksonTest.class.getClassLoader().getResource("testdata/user.json");
-		System.out.println(url);
+	public void testSimpleFromString() {
+		//System.out.println(getJson());
 		User user = null;
 		try {
-			user = mapper.readValue(new File(url.getFile()), User.class);
-			//user = mapper.readValue(getJson(), User.class);
+			user = mapper.readValue(getJson(), User.class);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(user);
+		try {
+			mapper.writeValue(new File("target/user-modified.json"), user);
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Ignore
+	@Test
+	public void testSimpleFromFile() {
+		//System.out.println(getJson());
+		URL url = JacksonTest.class.getClassLoader().getResource("testdata/user.json");
+		System.out.println("FILE IS: " + url);
+		File theUserJsonFile = new File(url.getFile());
+
+		User user = null;
+		try {
+			//System.out.println(userJsonFile.getAbsolutePath());
+			user = mapper.readValue(theUserJsonFile, User.class);
 		} catch (JsonParseException e) {
 			e.printStackTrace();
 		} catch (JsonMappingException e) {
