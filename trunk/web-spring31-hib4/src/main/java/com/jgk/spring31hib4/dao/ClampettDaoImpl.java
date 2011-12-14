aa3e7eb434d141ec08f5c7af391828a495ced6f3
javapda@gmail.com <javapda@gmail.com@c572b21e-de6c-5e44-9ac8-d7cb41f125f7>
2011-12-14 01:30:06 +0000

    test transactions

---
+++ trunk/web-spring31-hib4/src/main/java/com/jgk/spring31hib4/dao/ClampettDaoImpl.java
@@ -0,0 +1,40 @@
+package com.jgk.spring31hib4.dao;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.jgk.spring31hib4.domain.Granny;
+import com.jgk.spring31hib4.domain.Jed;
+
+@Component
+public class ClampettDaoImpl implements ClampettDao {
+    protected transient final Log log = LogFactory.getLog(ClampettDaoImpl.class);
+            
+    @Autowired SessionFactory sessionFactory;
+    
+    private Session getCurrentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+    public Granny getGrannyById(Integer id) {
+        if (log.isDebugEnabled()) {
+            log.debug("loading by id: " + id);
+        }
+        return (Granny) getCurrentSession().get(Granny.class, id);
+    }
+    public Jed getJedById(Integer id) {
+        if (log.isDebugEnabled()) {
+            log.debug("loading by id: " + id);
+        }
+        return (Jed) getCurrentSession().get(Jed.class, id);
+    }
+    @Override
+    public void save(Object entity) {
+        getCurrentSession().save(entity);
+        
+    }
+
+}
