7da301e864e2e7db3ec269f24d84ce402debc951
javapda <javapda@gmail.com>
2012-01-07 23:16:27 +0000

    update domain

---
+++ web-spring31-hib4/src/main/java/com/jgk/spring31hib4/domains/clampett/Granny.java
@@ -0,0 +1,61 @@
+package com.jgk.spring31hib4.domains.clampett;
+
+import java.util.Date;
+
+public class Granny {
+    private Integer grannyId;
+    private Date birthdate;
+    private String firstName,lastName;
+    private Jed jed;
+    
+    
+    public Jed getJed() {
+        return jed;
+    }
+
+    public void setJed(Jed jed) {
+        this.jed = jed;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getGrannyId() {
+        return grannyId;
+    }
+
+    public void setGrannyId(Integer grannyId) {
+        this.grannyId = grannyId;
+    }
+
+    public static Granny createGranny(String _firstName, String _lastName,
+            Date _birthdate) {
+        Granny granny = new Granny();
+        granny.setFirstName(_firstName);
+        granny.setLastName(_lastName);
+        granny.setBirthdate(_birthdate);
+        return granny;
+    }
+
+}
