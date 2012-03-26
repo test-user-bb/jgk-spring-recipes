cce923a2f29022ac6d301b13edaf13ca1e515021
javapda <javapda@gmail.com>
2012-03-26 15:45:41 +0000

    add longs

--- jgk-util/src/test/java/com/jgk/util/io/file/WriteBinaryFileTest.java
+++ jgk-util/src/test/java/com/jgk/util/io/file/WriteBinaryFileTest.java
@@ -8,7 +8,19 @@ import org.junit.Test;
 
 public class WriteBinaryFileTest {
 
-	@Test public void testit() {
+	@Test public void testWriteLongs() {
+		WriteBinaryFile wbf = new WriteBinaryFile();
+		File targetDir = new File("target");
+		if (!targetDir.exists()) {
+			throw new RuntimeException("Missing target directory: "+targetDir.getAbsolutePath());
+		}
+		File file = new File(targetDir,"testBinaryLong.dat");
+		wbf.writeRandomLongs(file, 1);
+		Assert.assertEquals(8, file.length());
+		file.delete();
+		Assert.assertFalse(file.exists());
+	}
+	@Test public void testWriteInts() {
 		WriteBinaryFile wbf = new WriteBinaryFile();
 		File targetDir = new File("target");
 		if (!targetDir.exists()) {
