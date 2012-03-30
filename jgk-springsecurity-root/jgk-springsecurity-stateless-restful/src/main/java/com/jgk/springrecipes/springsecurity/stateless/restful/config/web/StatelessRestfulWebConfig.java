fe427e5a8835e6fc8291aec677f242a7fd95266c
javapda <javapda@gmail.com>
2012-03-30 23:04:57 +0000

    add one

--- jgk-springsecurity-root/jgk-springsecurity-stateless-restful/src/main/java/com/jgk/springrecipes/springsecurity/stateless/restful/config/web/StatelessRestfulWebConfig.java
+++ jgk-springsecurity-root/jgk-springsecurity-stateless-restful/src/main/java/com/jgk/springrecipes/springsecurity/stateless/restful/config/web/StatelessRestfulWebConfig.java
@@ -1,8 +1,10 @@
 package com.jgk.springrecipes.springsecurity.stateless.restful.config.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 public class StatelessRestfulWebConfig {
     public StatelessRestfulWebConfig() {
         System.out.println("StatelessRestfulWebConfig");
