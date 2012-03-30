33588c805166fd0a6833aa4c56f78c00c89f11aa
javapda@gmail.com <javapda@gmail.com@c572b21e-de6c-5e44-9ac8-d7cb41f125f7>
2012-03-30 23:52:53 +0000

    add simple controller

--- trunk/jgk-springsecurity-root/jgk-springsecurity-stateless-restful/src/main/java/com/jgk/springrecipes/springsecurity/stateless/restful/config/web/StatelessRestfulWebSecurityConfig.java
+++ trunk/jgk-springsecurity-root/jgk-springsecurity-stateless-restful/src/main/java/com/jgk/springrecipes/springsecurity/stateless/restful/config/web/StatelessRestfulWebSecurityConfig.java
@@ -1,10 +1,14 @@
 package com.jgk.springrecipes.springsecurity.stateless.restful.config.web;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
+import com.jgk.springrecipes.springsecurity.stateless.restful.controller.StatelessRestfulController;
+
 @Configuration
 @ImportResource(value={ "classpath*:jgk-springsecurity-stateless-restful-web-security-config.xml" })
+//@ComponentScan(basePackageClasses={ StatelessRestfulController.class })
 public class StatelessRestfulWebSecurityConfig {
     public StatelessRestfulWebSecurityConfig() {
         System.out.println("StatelessRestfulWebSecurityConfig");
