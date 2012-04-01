d3bf9490cef603710d969ff6ffa85b26ab980c31
javapda <javapda@gmail.com>
2012-04-01 18:21:42 +0000

    update rest

--- jgk-springsecurity-root/jgk-springsecurity-stateless-restful/src/main/java/com/jgk/springrecipes/springsecurity/stateless/restful/config/web/StatelessRestfulWebConfig.java
+++ jgk-springsecurity-root/jgk-springsecurity-stateless-restful/src/main/java/com/jgk/springrecipes/springsecurity/stateless/restful/config/web/StatelessRestfulWebConfig.java
@@ -1,10 +1,14 @@
 package com.jgk.springrecipes.springsecurity.stateless.restful.config.web;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.jgk.springrecipes.springsecurity.stateless.restful.beans.MyRestAuthenticationEntryPoint;
+
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackageClasses={ MyRestAuthenticationEntryPoint.class })
 public class StatelessRestfulWebConfig {
     public StatelessRestfulWebConfig() {
         System.out.println("StatelessRestfulWebConfig");
