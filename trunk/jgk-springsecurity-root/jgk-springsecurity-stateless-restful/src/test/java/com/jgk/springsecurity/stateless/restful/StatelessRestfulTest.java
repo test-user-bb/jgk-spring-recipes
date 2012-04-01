55a93b48f897947a60f71cb155c9c318f27ca1fb
javapda@gmail.com <javapda@gmail.com@c572b21e-de6c-5e44-9ac8-d7cb41f125f7>
2012-04-01 18:21:42 +0000

    update rest

---
+++ trunk/jgk-springsecurity-root/jgk-springsecurity-stateless-restful/src/test/java/com/jgk/springsecurity/stateless/restful/StatelessRestfulTest.java
@@ -0,0 +1,40 @@
+package com.jgk.springsecurity.stateless.restful;
+import static com.jayway.restassured.RestAssured.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Header;
+import com.jayway.restassured.response.Headers;
+import com.jayway.restassured.response.Response;
+
+public class StatelessRestfulTest {
+    
+    @Test public void tester() {
+        Response r = RestAssured.given().auth().preemptive().basic("rest", "rest").get("http://localhost:8080/jgk-springsecurity-stateless-restful/rest/api/help/", new HashMap<String,String>());
+        System.out.println(r.asString());
+    }
+    private String interrogateResponse(Response r) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(r.asString());
+        //expect().body("lotto.lottoId", equalTo(5)).when().get("/gs-deploy");
+     // Get all headers
+        Headers allHeaders = r.getHeaders();
+        for (Header header : allHeaders) {
+            sb.append("|");
+            sb.append("header:"+header);
+        }
+     // Get all cookies as simple name-value pairs
+        Map<String, String> allCookies = r.getCookies();
+        sb.append("Cookies: " + allCookies);
+        
+        int statusCode = r.getStatusCode();
+        sb.append("|");
+        sb.append("Status code: "+statusCode);
+        return sb.toString();
+    }
+
+}
